2287258e0a9bc534599a461ae6637272f3d5c315
Emilio López <emilio.lopez@trailofbits.com>
2024-02-14 00:37:25 +0000

    manticore: HACK: temporarily use `fork` on mp
    Python 3.8 on Darwin switched from `fork` to `spawn` and it breaks
    Manticore. Temporarily roll back to `fork`.

--- manticore/__init__.py
+++ manticore/__init__.py
@@ -1,4 +1,5 @@
 import sys
+import multiprocessing
 
 if sys.version_info < (3, 8):
     print("Manticore requires Python 3.8 or higher.")
@@ -18,3 +19,7 @@ __all__ = [
     set_verbosity.__name__,
     ManticoreError.__name__,
 ]
+
+# HACK: darwin switched from fork to spawn on Python 3.8 and it breaks manticore
+# Temporarily revert it to the previous behavior
+multiprocessing.set_start_method("fork")
